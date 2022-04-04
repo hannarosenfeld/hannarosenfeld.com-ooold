aea6f37a79aaea00bb36315dd533cd84291f5d05
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-04-04 15:49:14 -0400

    Add typewriter effect to bio.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,27 @@
-import * as React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'gatsby'
+import Typewriter from "typewriter-effect";
 
 import '../styles/base.css'
 
-
+function TypewriterEffect() {
+  return (
+    <div className="typewriter">
+      <Typewriter
+       onInit={(typewriter)=> {
+       typewriter
+       .typeString("Hello World!")
+       .pauseFor(1000)
+       .deleteAll()
+       .typeString("I am a web developer living and working in Philadelphia, PA.")
+       .typeString(" Contact me through my social links below: ")
+       .pauseFor(1000)
+       .start();
+       }}
+       />
+    </div>
+  );
+}
 const IndexPage = () => {
     return (
         <>
@@ -11,6 +29,8 @@ const IndexPage = () => {
                 <h1>
                     <Link className="hzr" to="/">&lt;hzr/&gt;</Link>
                 </h1>
+                <TypewriterEffect/>
+                <p id="typewriter"></p>
                 <div>
                     <Link className="social"to="https://github.com/hannarosenfeld">&gt; github</Link>
                 </div>
