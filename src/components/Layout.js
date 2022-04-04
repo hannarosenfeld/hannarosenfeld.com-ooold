cf57a79f776b2dc22a7136b2e9885abf7c948e8d
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-04-04 19:07:42 -0400

    Show social links when typewriter is done writing.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,42 +1,24 @@
 import React, { useEffect } from 'react'
 import { Link } from 'gatsby'
-import Typewriter from "typewriter-effect";
 
 import '../styles/base.css'
+import Intro from '../components/Intro'
 
-function TypewriterEffect() {
-  return (
-    <div className="typewriter">
-      <Typewriter
-       onInit={(typewriter)=> {
-       typewriter
-       .typeString("Hello World!")
-       .pauseFor(1000)
-       .deleteAll()
-       .typeString("I am a web developer living and working in Philadelphia, PA.")
-       .typeString(" Contact me through my social links below: ")
-       .pauseFor(1000)
-       .start();
-       }}
-       />
-    </div>
-  );
-}
-const IndexPage = () => {
+
+const Layout = ({ children }) => {
     return (
         <>
             <header>
                 <h1>
                     <Link className="hzr" to="/">&lt;hzr/&gt;</Link>
                 </h1>
-                <TypewriterEffect/>
-                <p id="typewriter"></p>
-                <div>
-                    <Link className="social"to="https://github.com/hannarosenfeld">&gt; github</Link>
-                </div>
+                <Intro/>
             </header>
+            <div>
+                {children}
+            </div>
         </>
     )
 }
 
-export default IndexPage
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,11 @@ import * as React from "react"
 
 import Layout from '../components/Layout'
 
+
 const IndexPage = () => {
-  return (
-      <Layout/>
-  )
+    return (
+        <Layout/>
+    )
 }
 
 export default IndexPage
