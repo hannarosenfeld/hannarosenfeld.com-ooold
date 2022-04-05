772c993b51206eda05ce308f6b54ec59de66f5b1
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2022-04-04 20:46:42 -0400

    Add google analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,28 @@
 module.exports = {
-  siteMetadata: {
-    title: `new`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
+    siteMetadata: {
+        title: `new`,
+        siteUrl: `https://hannarosenfeld.com/`,
+    },
+    plugins: [
+        {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-MRJP8QS9GZ", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: "G-MRJP8QS9GZ",
+            },
+        },
+    ],
 }
