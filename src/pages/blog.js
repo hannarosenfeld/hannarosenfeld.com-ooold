90eca127878beb9467810aae1be5a714e4386887
Hanna Rosenfeld <hannazrosenfeld@gmail.com>
2023-01-06 08:59:31 -0500

    Delete.

---
+++ src/pages/blog.js
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+export default function Component () {
+  return (
+    <main
+      style={{width: "600px", margin: "0 auto"}}
+    >
+      <div>
+        i have a break from the bootcamp until the 2nd of january or the 3rd
+
+        i am back in the discord and on wireclub
+
+        i deleted my instagram and twitter
+
+        i am on facebook but mostly because i plan to use it for housing
+
+        it's actually really good for that
+      </div>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/22JOEHhQM1I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </main>
+  )
+}
---
+++ src/pages/notes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Test = () => (
+    <div>
+      <p>Hook this up to strapi.</p>
+      <p>Math162</p>
+    </div>
+)
+
+export default Test
